bd672c38a7a6456762231e1429e9dec1db7f57fb
hsw824 <ghdtjddn0824@naver.com>
2022-06-04 20:46:38 +0900

    Feat: Add a DiaryList

---
+++ src/routes/DiaryList/index.tsx
@@ -0,0 +1,25 @@
+import styles from './diaryList.module.scss'
+import DiaryItem from './DiaryItem'
+import useLocalStorageState from 'use-local-storage-state'
+import Footer from 'routes/_shared/Footer'
+
+const DiaryList = () => {
+  const [editData] = useLocalStorageState<any[]>('editData', {
+    ssr: true,
+    defaultValue: [],
+  })
+  return (
+    <div className={styles.listContainer}>
+      <h2> 일기 리스트</h2>
+      <h4>{editData.length}개의 일기가 있습니다.</h4>
+      <div>
+        {editData.map((item: any) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default DiaryList
